e1f366be069dcd0c53e109b118a69ec1c69ac409
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2023-08-09 19:33:22 +0530

    Update function.php

--- web1/config/function.php
+++ web1/config/function.php
@@ -1,4 +1,32 @@
 <?php
+session_start();
+if (isset($_POST['submit'])) {
+    $phone = isset($_POST["phone"]) ? $_POST["phone"] : '';
+    $password = isset($_POST["password"]) ? $_POST["password"] : '';
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "loginpage";
+
+    $conn = mysqli_connect($host, $user, $pass, $dbname);
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE phone = $phone");
+    if (!$result || mysqli_num_rows($result) == 0) {
+?>
+        <script>
+            alert("User Does Not Exist");
+        </script>
+<?php
+    } else {
+        $userData = array(
+            "userId" => $result['id'],
+            "role" => 'user',
+        );
+
+        $_SESSION['user'] = $userData;
+    }
+}
+
+
 function pre($item)
 {
     echo "
