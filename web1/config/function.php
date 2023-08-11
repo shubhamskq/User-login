c8c4954fcca23560d9674189e41ad06073fb2204
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2023-08-11 19:07:25 +0530

    Update function.php

--- web1/config/function.php
+++ web1/config/function.php
@@ -1,32 +1,6 @@
 <?php
+
 session_start();
-if (isset($_POST['submit'])) {
-    $phone = isset($_POST["phone"]) ? $_POST["phone"] : '';
-    $password = isset($_POST["password"]) ? $_POST["password"] : '';
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $dbname = "loginpage";
-
-    $conn = mysqli_connect($host, $user, $pass, $dbname);
-    $result = mysqli_query($conn, "SELECT * FROM user WHERE phone = $phone");
-    if (!$result || mysqli_num_rows($result) == 0) {
-?>
-        <script>
-            alert("User Does Not Exist");
-        </script>
-<?php
-    } else {
-        $userData = array(
-            "userId" => $result['id'],
-            "role" => 'user',
-        );
-
-        $_SESSION['user'] = $userData;
-    }
-}
-
-
 function pre($item)
 {
     echo "
@@ -39,4 +13,42 @@ function base_url($arg = NULL)
 {
     $base = 'http://localhost/Shubham/web1/';
     return empty($arg) ? $base : $base . $arg;
+}
+
+//Check if user login
+function isUserLogin()
+{
+    if (isset($_SESSION['userId']) && !empty($_SESSION['userId'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//Check if User login in login page
+function checkUserLogin()
+{
+    if (isset($_SESSION['userId']) && !empty($_SESSION['userId'])) {
+        return true;
+    } else {
+        header("Location:" . base_url('login'));
+    }
+}
+//Check if user login while updating
+function userUpdate()
+{
+    if (isset($_SESSION['userId']) && !empty($_SESSION['userId'])) {
+        return true;
+    } else {
+        header("Location:" . base_url('login'));
+    }
+}
+
+function imageSubmit($placeholder, $name, $value, $type)
+{
+    $ele = "
+    <div class = \"form my-4\">
+    <input type='$type' name='$name' placeholder='$placeholder' value='$value'
+    class= \"form-control\">
+    ";
+    echo $ele;
 }
